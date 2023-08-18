f8b7d994897608aea82fdeec58a00450efdc53e0
Xaviour Greenhalgh <xaviourgreenhalgh@gmail.com>
2023-08-18 11:20:57 +1000

    implement colours

--- client/src/Colours.ts
+++ client/src/Colours.ts
@@ -1,16 +1,23 @@
 const colours = {
-  primary: '',
-  secondary: '',
+  gradients: {
+    green: {
+      start: '#00EDB4',
+      end: '#2CDC10',
+    },
+    purple: {
+      start: '#00FFFF',
+      end: '#FA00FF',
+    },
+    red: {
+      start: '#FF007A',
+      middle: '#D8AF1D',
+      end: '#FF0000',
+    },
+  },
   neutral: {
     light: '',
     mid: '',
     dark: '',
-  },
-  text: {
-    title: '',
-    subtitle: '',
-    body: '',
-    highlight: '',
   },
   background: {
     dark: '#000000',
